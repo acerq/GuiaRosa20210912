0f0560b86f9a39c353c853081e3fa4786e9ee4c4
Glitch (locrian-delightful-constellation) <none>
2020-07-17 20:35:26 +0000

    😎🗃 Checkpoint ./public/scripts/app.js:10731577/7 ./public/menu_medico.html:10731577/10 ./public/menu_paciente.html:10731577/8

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -393,7 +393,7 @@ function renderSolicitacao(resposta) {
   console.log("(app.js) renderSolicitacao -> ", resposta);
   if(resposta.mensagem == "Ok")	{
 	  alert("Exame agendado com sucesso");
-  	history.go(-1);
+  	window.history.go(-1);
   } else {
 	  alert(resposta.mensagem);	  
   }
