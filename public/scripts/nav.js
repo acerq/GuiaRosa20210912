8bbe61be40fd9e18c705f3751865ebc8d131058d
Glitch (guia-rosa) <none>
2020-07-30 23:20:09 +0000

    👩‍👩‍👦🌦 Checkpoint ./public/inicio.html:10731577/101 ./public/listas.html:10731577/3516 ./public/scripts/nav.js:10731577/700 ./public/menu_medico.html:10731577/288 ./public/menu_paciente.html:10731577/868

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -174,6 +174,28 @@ function solicitacaoDeExames() {
 
 // -----------------------------------------------------------------------------------------//
 
+function apresentarListas() {
+  console.log("(nav.js) Verificando Timeout ");
+  return fetch("/verificarTimeout")
+    .then(async response => {
+      console.log("(nav.js) VerificarTimeout response");
+      let msg = await response.json();
+      if (msg.hasOwnProperty("erro")) {
+        alert(msg.erro);
+        window.location.href = "index.html";
+        return;
+      }
+      window.location.href = "listas.html";
+      return response.json();
+    })
+    .catch(e => {
+      console.log("(nav.js) do catch", e);
+      return null;
+    });
+}
+
+// -----------------------------------------------------------------------------------------//
+
 window.retornarUsrApp = function() {
   return usrApp;
 };
