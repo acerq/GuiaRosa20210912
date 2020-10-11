c311a18e23b4c77c595bf54dae5dbee9883dab19
Glitch (guia-rosa) <none>
2020-10-11 16:28:19 +0000

    💳🏢 Checkpoint ./public/pgto.html:10731577/11689 ./public/scripts/pgto.js:10731577/60

--- public/scripts/pgto.js
+++ public/scripts/pgto.js
@@ -9,7 +9,6 @@ var store = null;
 var transacao = null;
 var usr = null;
 
-const divConteudo = document.getElementById("divConteudo");
 const divInstrucao = document.getElementById("divInstrucao");
 const tfNomeCartao = document.getElementById("tfNomeCartao");
 const tfNumCartao = document.getElementById("tfNumCartao");
