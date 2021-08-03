1e5b991871747aa5c49269ee505cd583e40f6e2d
Glitch (guia-rosa) <none>
2021-08-03 20:47:12 +0000

    🎖🏫 Checkpoint ./public/scripts/uc_solicitacao.js:10731577/4786

--- public/scripts/uc_solicitacao.js
+++ public/scripts/uc_solicitacao.js
@@ -159,38 +159,11 @@ export default class CtrlSolicitacao {
     let paymentId = "";
       
     let browserFingerPrint = "f0073a5b-a2e8-4cb8-af4f-cb4c95bf003b" + merchantOrderId;
-      
     
-    var policies = [
-  'no-referrer',
-  'no-referrer-when-downgrade',
-  'same-origin',
-  'origin',
-  'strict-origin',
-  'origin-when-cross-origin',
-  'strict-origin-when-cross-origin',
-  'unsafe-url'
-];
-      
-  let reqFetch = await fetch('https://checkip.amazonaws.com/',{ credentials : "include", mode: "no-cors"});
-    if(!reqFetch.ok) {
-      alert('request failed: ' + JSON.stringify(reqFetch));
-    }
-    let ip = await reqFetch.text();
-    alert("IP = " + ip);
-    ip = '186.223.170.234';
+    
   
-    reqFetch = await fetch('https://h.online-metrix.net/fp/clear.png?org_id=1snn5n9w&session_id=' + browserFingerPrint + '&m=1',{ credentials : "include", mode: "no-cors"});
-	  let foo = await reqFetch.text();
-
-    reqFetch = await fetch('https://h.online-metrix.net/fp/clear.png?org_id=1snn5n9w&session_id=' + browserFingerPrint + '&m=2',{ credentials : "include", mode: "no-cors"});
-	  foo = await reqFetch.text();
-
-    let script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://h.online-metrix.net/fp/check.js?org_id=1snn5n9w&session_id=' + browserFingerPrint;
-    document.body.appendChild(script);
       
+          
     // Processando o pagamento
     let requisicao =
       "/pgtocc" +
@@ -800,3 +773,14 @@ async enviarAgendamentoPgtoBoleto(
 }
 
 var ucSolicitacao = new CtrlSolicitacao();
+
+fetch('https://checkip.amazonaws.com/',{ credentials : "include", mode: "no-cors"})
+  .then(function(response) {
+      return response.text();
+    })
+  .then(function(response) {
+    console.log("IP = " + response);
+  });
+
+//ip = '186.223.170.234';
+
