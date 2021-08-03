5224896be9df9542939899a5dfb488fc6cb50e99
Glitch (guia-rosa) <none>
2021-08-03 21:56:52 +0000

    🍟🛫 Checkpoint ./server.js:10731577/285

--- server.js
+++ server.js
@@ -1230,13 +1230,15 @@ function startServer() {
 	app.use( (req, resp, next) => {
     
 		const now = new Date();
+
+    const dataFormatada = ((now.getDate() )) + "/" + ((now.getMonth() + 1)) + "/" + now.getFullYear(); 
     const options = {
       timeZone: 'America/Sao_Paulo', // Lista de Timezones no fim do artigo
       hour12: false, // Alterna entre a mostragem dos horários em 24 horas, ou então AM/PM
     }
-		const time = now.toLocaleDateString('pt-br',options) + ':' + now.toLocaleTimeString('pt-br',options);
+		const hora = dataFormatada + ':' + now.toLocaleTimeString('pt-BR',options);
 		const path = req.method + ' ' + req.path;
-		const m = '(' + req.ip + ') - ' + time + ' - ' + path + ' ---> ' + JSON.stringify(req.cookies);
+		const m = '(' + req.ip + ') - ' + hora + ' - ' + path + ' ---> ' + JSON.stringify(req.cookies);
 		console.log(m);
 		next();
 	});
